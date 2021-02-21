d6e097f262e801848ca41faad46ea6e7fda16201
healing99 <nicealice99@gmail.com>
2021-02-21 22:11:33 +0900

    create react app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+//getElementById("root") 내부에 App.js의 내용을 넣어줌
