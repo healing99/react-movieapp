c557a362b9f6b60511d0e4b3be64a429dd131e8e
healing99 <nicealice99@gmail.com>
2021-02-23 01:00:21 +0900

    fetching movie infos from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    //axios가 끝날때까지 기다려
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    // console.log(movies);
+    this.setState({ movies, isLoading: false }); //this.setState({movies: movies})
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className={movies}>
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
 export default App;
